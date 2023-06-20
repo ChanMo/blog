9f6d9461300c2c96a96954340f341faf67a25363
dyou <chen.orange@aliyun.com>
2023-06-20 21:40:37 +0800

    add adsense js

--- img/cli.py
+++ img/cli.py
@@ -6,13 +6,20 @@ from PIL import Image
 width = 960
 
 @click.command()
-def cli():
-    for infile in glob.glob("*.png"):
-        file, _ = os.path.splitext(infile)
-        with Image.open(infile) as im:
-            im = im.convert('RGB')
-            im.thumbnail((width,width / im.size[0] * im.size[1]))
-            im.save(file + ".jpeg", "JPEG")
+@click.option('--img', prompt='The original image')
+@click.option('--width', type=int, default=960)
+def cli(img, width):
+    # for infile in glob.glob("*.png"):
+    #     file, _ = os.path.splitext(infile)
+    #     with Image.open(infile) as im:
+    #         im = im.convert('RGB')
+    #         im.thumbnail((width,width / im.size[0] * im.size[1]))
+    #         im.save(file + ".jpeg", "JPEG")
+    filename, _ = os.path.splitext(img)
+    with Image.open(img) as im:
+        im = im.convert('RGB')
+        im.thumbnail((width,width / im.size[0] * im.size[1]))
+        im.save(filename + ".jpeg", "JPEG")
             
             
 
